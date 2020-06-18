0e8d6f7cb9dcb713cd76413ce273908f7423b77e
Tar <tarsiciosh@gmail.com>
2020-06-18 15:33:44 -0300

    atom mac

--- App.js
+++ App.js
@@ -1,37 +1,97 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button 
+        title="Go to Details"  
+        onPress={ () => { 
+          //1. Navigate to the Details route with params
+          navigation.navigate('Details' , {
+            //itemId : 86,
+            otherParam: 'anything you want here!!'  
+          })   
+        }} 
+      />
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({route, navigation}) {
+  //2. Get the params
+  const {itemId} = route.params
+  const {otherParam} = route.params
+
+  //3. Usse the params
   return(
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)} </Text>
+      <Text>otherParam: {JSON.stringify(otherParam)} </Text>
+      <Button 
+        title= "Go to Details... again"
+        onPress={ () => 
+          navigation.push('Details' , {
+            itemId: Math.floor(Math.random() * 100),
+            otherParam: "nested details"
+          })
+        } 
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={()=> navigation.goBack()} />
+      <Button 
+        title="Go back to first screen in stack"
+        onPress= {() => navigation.popToTop()} 
+      />
     </View> 
   )
 }
 
 const Stack = createStackNavigator();
 
+
 function App() {
-  return (
+  return ( // screenOptions={/**/}
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} /> 
+      <Stack.Navigator initialRouteName={"Home"} >
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{title: 'Overview'}} 
+        />
+        <Stack.Screen 
+          name="Details" 
+          component={DetailsScreen} 
+          initialParams={{ itemId: 42 }}
+        /> 
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+//If you didn't specify any params when navigating to this screen, 
+//the initial params will be used. They are also shallow merged with
+//any params that you pass. 
+
+
 export default App;
+
+/*
+Lecture example:
+$ mkdir screens
+$ touch AddContactScreen.js
+$ mv AddContactScreen.js screens/
+$ touch creens/ContactListScreen.js
+
+(App.js)
+<AppNavigator screenProps={{contacts: this.state.contacts }} />
+
+(ContactListScreen.js)
+<SectionListContacts contacts={this.props.screenProps.contacts} />
+
+-also pases the addcontact function
+*/
